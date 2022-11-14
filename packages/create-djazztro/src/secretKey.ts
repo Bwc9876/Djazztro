9992ea5b43c109f894d4d1f607708dd8c6a0bb12
Bwc9876 <Bwc9876@users.noreply.github.com>
2022-11-14 13:28:23 +0000

    Format

--- packages/create-djazztro/src/secretKey.ts
+++ packages/create-djazztro/src/secretKey.ts
@@ -6,9 +6,9 @@ const CHARS = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)".split("");
 const KEY_LENGTH = 50;
 
 export function getSecretKey() {
-  const key = [];
-  for (let i = 0; i < KEY_LENGTH; i++) {
-    key.push(CHARS[Math.floor(Math.random() * CHARS.length)]);
-  }
-  return key.join("");
+    const key = [];
+    for (let i = 0; i < KEY_LENGTH; i++) {
+        key.push(CHARS[Math.floor(Math.random() * CHARS.length)]);
+    }
+    return key.join("");
 }
